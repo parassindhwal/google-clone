46a851738352541c9df943c9263057c9c5d82bf2
Paras <parassindhwal004@gmail.com>
2021-10-22 23:50:17 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Search } from './Search';
+import { useResultContext } from '../contexts/ResultContextProvider';
+
+export const Navbar = ({setDarkTheme, darkTheme}) => {
+    const {setResults, setSearchTerm} = useResultContext();
+    return (
+        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center item-center border-b dark:border-gray-700 border-gray-200">
+            <div className="flex justify-between item-center spaxe-x-5 w-screen">
+                <Link onClick={() => {setResults([]); setSearchTerm('')}} to="/">
+                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+                        Google 	&#128269;
+                    </p>
+                </Link>
+                <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg">
+                    {darkTheme ? 'Light 💡' : 'Dark 🌙'}
+                </button>
+            </div>
+            <Search />
+        </div>
+    )
+}
